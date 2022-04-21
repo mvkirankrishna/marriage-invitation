faa8a54c049251346e1ec4482e6141727230909c
kmutyala <krishna.mutyala@q2.com>
2022-04-21 00:42:49 -0500

    Marriage Invitation

--- js/timer.js
+++ js/timer.js
@@ -4,24 +4,10 @@
         hour = minute * 60,
         day = hour * 24;
 
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "09/30/",
-      birthday = dayMonth + yyyy;
+  let bigday = "05/20/2022"
+
   
-  birthday = "05/20/2022"
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
-  //end
-  
-  const countDown = new Date(birthday).getTime(),
+  const countDown = new Date(bigday).getTime(),
       x = setInterval(function() {    
 
         const now = new Date().getTime(),
